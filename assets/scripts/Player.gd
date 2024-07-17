581ca274730036be74efb49f0bfabc9d7da7ca24
adilson-strider <adilsonjunior.dev01@gmail.com>
2024-07-17 15:36:48 -0300

    shoot

--- assets/scripts/Player.gd
+++ assets/scripts/Player.gd
@@ -1,8 +1,23 @@
 extends Area2D
 
+var plBullet := preload("res://assets/scenes/Bullet.tscn")
+
+onready var firingPositions := $FiringPositions
+onready var fireDelayTimer := $Timer
+
 export var speed: float = 100
+export var fireDelay: float = 0.5
 var vel := Vector2(0, 0)
 
+func _process(delta):
+	
+	# Tiro
+	if Input.is_action_pressed("shoot") and fireDelayTimer.is_stopped():
+		fireDelayTimer.start(fireDelay)
+		for child in firingPositions.get_children():
+			var bullet := plBullet.instance()
+			bullet.global_position = child.global_position
+			get_tree().current_scene.add_child(bullet)
 	
 func _physics_process(delta):
 	var dirVec := Vector2(0, 0)
@@ -19,8 +34,7 @@ func _physics_process(delta):
 	vel = dirVec.normalized() * speed
 	position += vel * delta
 	
-	# Restringe ao espaço da tela
+	# Nave dentro da cena (bounds)
 	var viewRect := get_viewport_rect()
 	position.x = clamp(position.x, 0, viewRect.size.x)
 	position.y = clamp(position.y, 0, viewRect.size.y)
-
