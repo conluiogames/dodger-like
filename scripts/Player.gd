f5d058ffaba937e16c276b70a89bab85cb8d7a6f
William <chimerarj@gmail.com>
2024-08-05 14:04:09 -0300

    Friction effect in player moviment

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -11,13 +11,11 @@ onready var firingPositions := $FiringPositions
 onready var fireDelayTimer := $Timer
 
 export var speed: float = 100
+export var acceleration: float = 400
+export var friction: float = 200
 export var fireDelay: float = 0.3
 export var life: int = 3
 var vel := Vector2(0, 0)
-
-func _ready():
-	pass
-
 func _process(delta):
 	# Tiro
 	if Input.is_action_pressed("shoot") and fireDelayTimer.is_stopped():
@@ -28,8 +26,8 @@ func _process(delta):
 			get_tree().current_scene.add_child(bullet)
 	
 func _physics_process(delta):
+	#Comandos de Movimentação
 	var dirVec := Vector2(0, 0)
-	
 	if Input.is_action_pressed("move_left") or Input.is_key_pressed(KEY_A):
 		dirVec.x = -1
 	elif Input.is_action_pressed("move_right") or Input.is_key_pressed(KEY_D):
@@ -39,7 +37,13 @@ func _physics_process(delta):
 	elif Input.is_action_pressed("move_down") or Input.is_key_pressed(KEY_S):
 		dirVec.y = 1
 
-	vel = dirVec.normalized() * speed
+	#efeito de atrito
+	dirVec = dirVec.normalized()
+	if dirVec != Vector2(0, 0):
+		vel = vel.move_toward(dirVec * speed, acceleration * delta)
+	else:
+		vel = vel.move_toward(Vector2(0, 0), friction * delta)
+	
 	position += vel * delta
 	
 	# Nave dentro da cena (bounds)
